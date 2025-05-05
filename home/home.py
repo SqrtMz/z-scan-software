bc837f49dddd42877f7a7746959b155737306903
SqrtMz <cvesgaduran@gmail.com>
2025-05-04 23:20:35 -0500

    Refactor Home class to enhance device selection and motor control functionality

--- home/home.py
+++ home/home.py
@@ -1,5 +1,6 @@
-from PySide6.QtWidgets import QMainWindow, QWidget, QPushButton, QHBoxLayout, QVBoxLayout, QLabel
-import serial.tools.list_ports
+from PySide6.QtWidgets import QMainWindow, QWidget, QPushButton, QHBoxLayout, QVBoxLayout, QLabel, QLineEdit
+from serial.tools import list_ports
+from serial import Serial
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg
 
@@ -7,7 +8,9 @@ class Home(QMainWindow):
     
     def __init__(self, app):
         super().__init__()
+
         self.app = app
+        self.device = None
 
         self.setWindowTitle("Home")
 
@@ -23,29 +26,85 @@ class Home(QMainWindow):
         w = QWidget()
         self.setCentralWidget(w)
 
-        self.text = QPushButton("Text")
+        layout = QHBoxLayout()
 
-        x = []
-        y = []
+        input_layout = QVBoxLayout()
+        layout.addLayout(input_layout)
+
+        self.selected_device_label = QLabel("Select a device")
+        input_layout.addWidget(self.selected_device_label)
+
+        motor_steps_layout = QHBoxLayout()
+        motor_steps_label = QLabel("Motor steps:")
+        self.motor_steps = QLineEdit()
+
+        input_layout.addLayout(motor_steps_layout)
+        motor_steps_layout.addWidget(motor_steps_label)
+        motor_steps_layout.addWidget(self.motor_steps)
+
+        actions_layout = QHBoxLayout()
+        input_layout.addLayout(actions_layout)
+
+        self.execute = QPushButton("Execute")
+        self.execute.clicked.connect(self.send_info_to_mcu)
+
+        self.go_to_start = QPushButton("Go to start")
+        self.go_to_start.clicked.connect(lambda: print("Go to start button clicked"))
+
+        self.go_to_end = QPushButton("Go to end")
+        self.go_to_end.clicked.connect(lambda: print("Go to end button clicked"))
+
+        actions_layout.addWidget(self.execute)
+        actions_layout.addWidget(self.go_to_start)
+        actions_layout.addWidget(self.go_to_end)
+
+        self.mcu_time = [0, 1, 2, 3, 4, 5]
+        self.mcu_voltage = [0, 1, 4, 9, 16, 25]
 
         fig = Figure()
         ax = fig.add_subplot()
-        ax.plot(x, y)
+        ax.plot(self.mcu_time, self.mcu_voltage)
         canvas = FigureCanvasQTAgg(fig)
 
-        layout = QHBoxLayout()
-        layout.addWidget(self.text)
         layout.addWidget(canvas)
 
         w.setLayout(layout)
+
     
     def reload_devices(self):
-        devices = [tuple(p)[0] for p in list(serial.tools.list_ports.comports())]
+
+        available_devices = [tuple(p)[0] for p in list(list_ports.comports())]
+        dev_actions = []
 
         self.devices_menu.clear()
 
-        if not devices:
+        if not available_devices:
              self.devices_menu.addAction("No devices connected").setEnabled(False)
         
-        for dev in devices:
-            self.devices_menu.addAction(dev)
+        for dev in available_devices:
+            dev_actions.append(self.devices_menu.addAction(dev))
+
+        for action in dev_actions:
+            action.triggered.connect(lambda s, dev=action: self.select_device(dev.text()))
+
+
+    def select_device(self, device):
+        self.device = device
+        self.selected_device_label.setText(f"Selected device: {device}")
+
+    
+    def send_info_to_mcu(self):
+
+        if self.device is None:
+            print("No device selected")
+            return
+
+        try:
+            steps = int(self.motor_steps.text())
+        except ValueError:
+            print("Invalid number of steps")
+            return
+        
+        ser = Serial(self.device, 115200)
+        ser.write(f"steps: {steps}".encode("utf-8"))
+        ser.close()
